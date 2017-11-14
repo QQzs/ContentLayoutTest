ac71ff3c0e4d94d166cc613b723aea9cb63fed2a
CodeZhang <552146383@qq.com>
2017-11-14 10:16:30 +0800

    添加展开收起的动画效果

--- app/src/main/java/com/zs/demo/contentlayouttest/MainActivity.java
+++ app/src/main/java/com/zs/demo/contentlayouttest/MainActivity.java
@@ -1,13 +1,16 @@
 package com.zs.demo.contentlayouttest;
 
+import android.animation.ValueAnimator;
 import android.app.Activity;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.TypedValue;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
@@ -20,24 +23,35 @@ import java.util.ArrayList;
 import java.util.List;
 
 
+/**
+ * @author zs
+ */
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private RelativeLayout layout_zhibo_intro;
     private LinearLayout ll_zhibo_intro;
     private LinearLayout ll_zhibo_intro_outline;
+    private TextView tv_title;
+    private LinearLayout ll_zhibo_intro_more;
+    private ImageView iv_zhibo_intro_more;
     private TextView tv_zhibo_intro_more;
 
-    private List<Integer> mData = new ArrayList<>();
-
-    // 简介是否展开  0：收起  1：展开
+    /**
+     * 简介是否展开  0：收起  1：展开
+     */
     private boolean mFlagShow;
-    // 简介默认高度  210dp
+    /**
+     * 简介默认高度  210dp
+     */
     private int mIntroHeight;
-    // 简介布局的高度
-    private int mLayoutHeight;
+    /**
+     * 简介全部内容的高度
+     */
+    private int mHeight = 0;
 
     private Activity mActivity;
     private ViewGroup.LayoutParams mParams;
+    private List<Integer> mData = new ArrayList<>();
 
     private String mText = " HBuilder是一款不错的开发工具，纵观，iOS开发的Xcode，Android开发的ADT、Studio，WP开发的VS。可以下载试玩，免环境安装，即可使用，内置Demo和教程。";
     private String mImage = "https://ss2.baidu.com/6ONYsjip0QIZ8tyhnq/it/u=4126321865,2582840083&fm=173&s=D096189A578546EEDE75B9D003009035&w=622&h=485&img.PNG";
@@ -52,13 +66,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         layout_zhibo_intro = (RelativeLayout) findViewById(R.id.layout_zhibo_intro);
         ll_zhibo_intro = (LinearLayout) findViewById(R.id.ll_zhibo_intro);
         ll_zhibo_intro_outline = (LinearLayout) findViewById(R.id.ll_zhibo_intro_outline);
+        tv_title = (TextView) findViewById(R.id.tv_title);
+        ll_zhibo_intro_more = (LinearLayout) findViewById(R.id.ll_zhibo_intro_more);
+        iv_zhibo_intro_more = (ImageView) findViewById(R.id.iv_zhibo_intro_more);
         tv_zhibo_intro_more = (TextView) findViewById(R.id.tv_zhibo_intro_more);
 
-        tv_zhibo_intro_more.setOnClickListener(this);
+        ll_zhibo_intro_more.setOnClickListener(this);
 
         mIntroHeight = DensityUtil.dip2px(mActivity, 210);
         mData.add(1);
         mData.add(2);
+        mData.add(1);
 
         setDetail();
 
@@ -76,21 +94,37 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     @Override
                     public void onGlobalLayout() {
                         ll_zhibo_intro.getViewTreeObserver().removeGlobalOnLayoutListener(this);
-                        mLayoutHeight = ll_zhibo_intro.getMeasuredHeight();
-                        if (mLayoutHeight <= mIntroHeight) {
-                            tv_zhibo_intro_more.setVisibility(View.GONE);
+                        mHeight = getLayoutHeight();
+                        if (mHeight <= mIntroHeight) {
+                            ll_zhibo_intro_more.setVisibility(View.GONE);
                         } else {
                             mFlagShow = false;
                             mParams.height = mIntroHeight;
                             mParams.width = ViewGroup.LayoutParams.MATCH_PARENT;
                             layout_zhibo_intro.setLayoutParams(mParams);
-                                tv_zhibo_intro_more.setVisibility(View.VISIBLE);
+                            ll_zhibo_intro_more.setVisibility(View.VISIBLE);
                             setMoreLayout();
                         }
                     }
                 });
     }
 
+    /**
+     * 获取全部内容的高度
+     * @return
+     */
+    private int getLayoutHeight(){
+        int height = tv_title.getMeasuredHeight() + DensityUtil.dip2px(this,28) + ll_zhibo_intro_outline.getChildCount() * DensityUtil.dip2px(this,8);
+        for (int i = 0; i< ll_zhibo_intro_outline.getChildCount(); i ++ ){
+            if (mData.get(i) == 1){
+                height += ll_zhibo_intro_outline.getChildAt(i).getMeasuredHeight();
+            }else{
+                // demo中图片高度，按实际为准
+                height += 485;
+            }
+        }
+        return height;
+    }
 
     /**
      * 添加内容
@@ -115,7 +149,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             for (int i = 0; i < intro.size(); i++) {
                 int bean = intro.get(i);
                 if (bean == 1) {
-                    params.width = LinearLayout.LayoutParams.MATCH_PARENT;
                     TextView textView = new TextView(this);
                     textView.setText(mText);
                     textView.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 14);
@@ -125,7 +158,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     layout.addView(textView);
 
                 } else if (bean == 2) {
-                    params.width = LinearLayout.LayoutParams.WRAP_CONTENT;
                     ImageView image = new ImageView(this);
                     // 当图片被盖住的时候，显示图片的顶部
                     image.setScaleType(ImageView.ScaleType.MATRIX);
@@ -147,19 +179,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View view) {
 
         switch (view.getId()){
-            case R.id.tv_zhibo_intro_more:
+            case R.id.ll_zhibo_intro_more:
                 if (mFlagShow) { // 收起
                     mFlagShow = false;
-                    ViewGroup.LayoutParams params = layout_zhibo_intro.getLayoutParams();
-                    params.height = mIntroHeight;
-                    params.width = ViewGroup.LayoutParams.MATCH_PARENT;
-                    layout_zhibo_intro.setLayoutParams(params);
+//                    ViewGroup.LayoutParams params = layout_zhibo_intro.getLayoutParams();
+//                    params.height = mIntroHeight;
+//                    params.width = ViewGroup.LayoutParams.MATCH_PARENT;
+//                    layout_zhibo_intro.setLayoutParams(params);
+                    animateClose(layout_zhibo_intro);
                 } else {    // 展开
                     mFlagShow = true;
-                    ViewGroup.LayoutParams params = layout_zhibo_intro.getLayoutParams();
-                    params.height = ViewGroup.LayoutParams.WRAP_CONTENT;
-                    params.width = ViewGroup.LayoutParams.MATCH_PARENT;
-                    layout_zhibo_intro.setLayoutParams(params);
+//                    ViewGroup.LayoutParams params = layout_zhibo_intro.getLayoutParams();
+//                    params.height = ViewGroup.LayoutParams.WRAP_CONTENT;
+//                    params.width = ViewGroup.LayoutParams.MATCH_PARENT;
+//                    layout_zhibo_intro.setLayoutParams(params);
+                    animateOpen(layout_zhibo_intro);
                 }
                 setMoreLayout();
                 break;
@@ -170,16 +204,82 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
      * 设置收起展开状态
      */
     private void setMoreLayout(){
-        Drawable drawable;
         if (mFlagShow){
             tv_zhibo_intro_more.setText("收起");
-            drawable = getResources().getDrawable(R.mipmap.zhibo_more_shouqi);
+            animationIvOpen();
         }else{
             tv_zhibo_intro_more.setText("展开");
-            drawable = getResources().getDrawable(R.mipmap.zhibo_more);
+            animationIvClose();
         }
+//        Drawable drawable;
+//        if (mFlagShow){
+//            tv_zhibo_intro_more.setText("收起");
+//            drawable = getResources().getDrawable(R.mipmap.zhibo_more_shouqi);
+//        }else{
+//            tv_zhibo_intro_more.setText("展开");
+//            drawable = getResources().getDrawable(R.mipmap.zhibo_more);
+//        }
         // 这一步必须要做,否则不会显示.
-        drawable.setBounds(0, 0, drawable.getMinimumWidth(), drawable.getMinimumHeight());
-        tv_zhibo_intro_more.setCompoundDrawables(drawable,null,null,null);
+//        drawable.setBounds(0, 0, drawable.getMinimumWidth(), drawable.getMinimumHeight());
+//        tv_zhibo_intro_more.setCompoundDrawables(drawable,null,null,null);
+    }
+
+    /**
+     * 展开布局
+     * @param view
+     */
+    private void animateOpen(View view) {
+        ValueAnimator animator = createDropAnimator(view, mIntroHeight,mHeight);
+        animator.start();
+    }
+
+    /**
+     * 收起布局
+     * @param view
+     */
+    private void animateClose(final View view) {
+        ValueAnimator animator = createDropAnimator(view, mHeight, mIntroHeight);
+        animator.start();
+    }
+
+    private ValueAnimator createDropAnimator(final View v, int start, int end) {
+        ValueAnimator animator = ValueAnimator.ofInt(start, end);
+        animator.setDuration(1000);
+        animator.setInterpolator(new AccelerateInterpolator());
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+
+            @Override
+            public void onAnimationUpdate(ValueAnimator arg0) {
+                int value = (int) arg0.getAnimatedValue();
+                ViewGroup.LayoutParams layoutParams = v.getLayoutParams();
+                layoutParams.height = value;
+                v.setLayoutParams(layoutParams);
+            }
+        });
+        return animator;
+    }
+
+    /**
+     * view展开 图标旋转动画
+     */
+    private void animationIvOpen() {
+        RotateAnimation animation = new RotateAnimation(0, 180,
+                Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF,
+                0.5f);
+        animation.setFillAfter(true);
+        animation.setDuration(300);
+        iv_zhibo_intro_more.startAnimation(animation);
+    }
+
+    /**
+     * view收起 图标旋转动画
+     */
+    private void animationIvClose() {
+        RotateAnimation animation = new RotateAnimation(180, 0,
+                Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF,
+                0.5f);
+        animation.setFillAfter(true);
+        animation.setDuration(200);
+        iv_zhibo_intro_more.startAnimation(animation);
     }
 }
